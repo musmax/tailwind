f0014bae786c6c4863f732be38b06a4853842c4c
musmax <musaojo1710@yahoo.com>
2024-11-12 03:27:26 +0100

    chore: added full code

---
+++ index.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    darkMode: 'selector',
+    extend: {
+        colors: {
+            "logo": {
+                700: "#1c59AE",
+                800: "#114a9a"
+            }
+        }
+    }
+}
